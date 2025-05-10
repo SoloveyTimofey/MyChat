f887b0c4044919ac45898ab8a30986a1e90233dd
Тимофей <116516370+SoloveyTimofey@users.noreply.github.com>
2025-05-10 09:55:23 +0300

    Add Identity features

--- src/MyChat.WebApp/Program.cs
+++ src/MyChat.WebApp/Program.cs
@@ -3,6 +3,9 @@ using MyChat.Core;
 using MyChat.Core.Models.Identity;
 using MyChat.DataAccess;
 using MyChat.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,9 +22,30 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     })
     .AddEntityFrameworkStores<MyChatDbContext>();
 
-builder.Services.AddRazorPages();
+builder.Services
+    .AddRazorPages()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("uk-UA")
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedUICultures = supportedCultures;
+});
 
 var app = builder.Build();
+
+app.UseRequestLocalization();
 
 if (!app.Environment.IsDevelopment())
 {
