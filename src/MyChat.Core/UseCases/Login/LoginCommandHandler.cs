c04408d7a0b126aeea02e8559425664f559e1563
Тимофей <116516370+SoloveyTimofey@users.noreply.github.com>
2025-05-10 10:29:34 +0300

    Add Localiztion

---
+++ src/MyChat.Core/UseCases/Login/LoginCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using MyChat.Core.Models.Identity;
+
+namespace MyChat.Core.UseCases.Login;
+
+internal sealed class LoginCommandHandler(
+    SignInManager<ApplicationUser> signInManager
+) : IRequestHandler<LoginCommand, Result>
+{
+    public async Task<Result> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var result = await signInManager.PasswordSignInAsync(
+            request.UserName,
+            request.Password,
+            isPersistent: false,
+            lockoutOnFailure: false
+        );
+
+        if (!result.Succeeded)
+        {
+            return Result.Error("Invalid email or password.");
+        }
+
+        return Result.Success();
+    }
+}
---
+++ src/MyChat.Core/UseCases/Register/RegisterCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using MyChat.Core.Models.Identity;
+
+namespace MyChat.Core.UseCases.Register;
+
+internal sealed class RegisterCommandHandler(
+    UserManager<ApplicationUser> userManager
+) : IRequestHandler<RegisterCommand, Result>
+{
+    public async Task<Result> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = request.UserName,
+            Email = request.Email,
+        };
+
+        var result = await userManager.CreateAsync(user, request.Password);
+
+        if (!result.Succeeded)
+        {
+            return Result.Error("Invalid data.");
+        }
+
+        return Result.Success();
+    }
+}
